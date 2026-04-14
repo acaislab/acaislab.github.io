c7a139855dfad80d0ae0ae40cc7c37daf8feaf77
acaislab <contacto@acaislab.com>
2026-04-14 11:22:49 -0400

    Eliminar

--- public/PlayColora_/workspace/get_clef.js
+++
@@ -1,6 +0,0 @@
-const https = require('https');
-https.get('https://raw.githubusercontent.com/musescore/MuseScore/master/fonts/bravura/bravura_metadata.json', (res) => {
-  let data = '';
-  res.on('data', (chunk) => data += chunk);
-  res.on('end', () => console.log(data.substring(0, 100)));
-});
