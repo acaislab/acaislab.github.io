7fdba0a17d99ab50694263ab5242662343eee3ea
acaislab <contacto@acaislab.com>
2026-08-04 10:35:53 -0400

    error por single

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Handle GitHub Pages SPA redirect
+const redirect = sessionStorage.redirect;
+if (redirect && redirect !== window.location.href) {
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, '', redirect);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
